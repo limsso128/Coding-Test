8a331c6a431d6255653def5b394bd24894f8e205
임소영 <s2412@e-mirim.hs.kr>
2025-02-27 18:40:23 +0900

    [level 0] Title: ad 제거하기, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,11 @@
+def solution(strArr):
+    answer = []
+    
+    for str in strArr:
+        if 'ad' not in str:
+            answer.append(str)
+            
+        else:
+            continue
+            
+    return answer
