c88a7e3d8d10941138355db1bc4d418527212b35
임소영 <s2412@e-mirim.hs.kr>
2025-04-03 23:25:15 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
@@ -1,10 +1,2 @@
 def solution(num_list):
-    answer = []
-    
-    num_list.sort()
-    
-    for i in range(len(num_list)):
-        if i < 5:
-            answer.append(num_list[i])
-            
-    return answer
+    return sorted(num_list)[:5]
