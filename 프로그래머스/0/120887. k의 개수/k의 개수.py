ca9cd9795557da3f8ecf7afe25b68572f448ea52
임소영 <s2412@e-mirim.hs.kr>
2025-06-05 23:20:37 +0900

    [level 0] Title: k의 개수, Time: 38.70 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,12 +1,5 @@
-def int_to_list(n):
-    return [int(d) for d in str(n)]
 def solution(i, j, k):
     answer = 0
-    
-    for l in range(i,j+1):
-        result = int_to_list(l)
-        for x in result:
-            if x == k:
-                answer += 1
-                
+    for n in range(i, j + 1):
+        answer += str(n).count(str(k))
     return answer
