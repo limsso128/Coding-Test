5f0d77081ea1a0c9cd841fd4d7100a388b0028f8
임소영 <s2412@e-mirim.hs.kr>
2025-06-08 22:58:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,11 +1,3 @@
 def solution(array):
-    answer = []
-    answer.append(max(array))
-    count = 0
-    
-    for i in array:
-        count += 1
-        if i == max(array):
-            answer.append(count-1)
-            
-    return answer
+    maxNum = max(array)
+    return [maxNum, array.index(maxNum)]
