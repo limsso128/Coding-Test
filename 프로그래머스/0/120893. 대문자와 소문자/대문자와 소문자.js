344e63798ef6e30765a3d1418c24f04e74b19822
임소영 <s2412@e-mirim.hs.kr>
2026-02-06 13:32:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    let answer = '';
+    
+    for(let x of my_string) {
+        if(x == x.toUpperCase()) {
+            answer += x.toLowerCase();
+        } else {
+            answer += x.toUpperCase();
+        }
+    }
+    
+    return answer;
+}
